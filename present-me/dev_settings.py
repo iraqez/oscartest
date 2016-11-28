74e437c9a785f3e6d84009e1ac4a88033c50eb98
Iraqez <vladimir.gumenyuk@gmail.com>
2016-11-28 21:36:26 +0200

    321

--- present-me/dev_settings.py
+++ present-me/dev_settings.py
@@ -2,7 +2,11 @@ from .settings import *
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': 'dev-present-me',
+    'USER': 'postgres',
+    'PASSWORD': 'workfree',
+    'HOST': 'localhost', # Set to empty string for localhost.
+    'PORT': '5432', # Set to empty string for default.
     }
 }
