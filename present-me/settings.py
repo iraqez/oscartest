8c6a54c53c6382251e2371bbab540af0646daa70
Iraqez <vladimir.gumenyuk@gmail.com>
2016-12-02 00:44:41 +0200

    dev

--- present-me/settings.py
+++ present-me/settings.py
@@ -74,7 +74,7 @@ INSTALLED_APPS = [
                    ])
 #
 SITE_ID = 1
-#
+#1
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
