bf855d07739e157b7147f8a93220c81696ff1e8b
Iraqez <vladimir.gumenyuk@gmail.com>
2016-12-02 00:49:53 +0200

    dev

--- osenv.py
+++ osenv.py
@@ -1,23 +1,3 @@
 import os
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-###
-osenv = os.environ.setdefault("DJANGO_SETTINGS_MODULE", "present-me.dev_settings")
-=======
 
 osenv = os.environ.setdefault("DJANGO_SETTINGS_MODULE", "present-me.settings")
->>>>>>> 1c13ab6c3bd7b8615b596df60af85891d1b0e989
-=======
-#
-osenv = os.environ.setdefault("DJANGO_SETTINGS_MODULE", "present-me.dev_settings")
->>>>>>> 9ec7991faf83d44bbde70411ae250c47bf8af4b3
-=======
-#
-osenv = os.environ.setdefault("DJANGO_SETTINGS_MODULE", "present-me.settings")
->>>>>>> d6ec04109399b5f2542bf3f10e0e4f922ee5459e
-=======
-
-osenv = os.environ.setdefault("DJANGO_SETTINGS_MODULE", "present-me.settings")
->>>>>>> 34217399c349e9c927401c94ea5a404a6e9d81aa
