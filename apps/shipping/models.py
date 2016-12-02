5bdc8259962d8d9d1b354084142b2d454bec8cf6
Iraqez <vladimir.gumenyuk@gmail.com>
2016-12-02 02:29:48 +0200

    dev

--- apps/shipping/models.py
+++ apps/shipping/models.py
@@ -1,1 +1,15 @@
+# apps/shipping/models.py
+
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from oscar.apps.shipping.abstract_models import AbstractOrderAndItemCharges
+
+
+class StandardMethod(AbstractOrderAndItemCharges):
+    """Модель с обычным набором атрибутов"""
+
+    class Meta(AbstractOrderAndItemCharges.Meta):
+        verbose_name = _('Shipping method')
+        verbose_name_plural = _('Shipping methods')
+
 from oscar.apps.shipping.models import *  # noqa
