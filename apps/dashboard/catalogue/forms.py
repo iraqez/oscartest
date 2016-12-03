736aa23e1128e6c7d270735491f33fb7a77bc5a1
Iraqez <vladimir.gumenyuk@gmail.com>
2016-12-03 22:14:26 +0200

    dev add keyword to category

---
+++ apps/dashboard/catalogue/forms.py
@@ -0,0 +1,8 @@
+from oscar.apps.dashboard.catalogue.forms import *  # noqa
+from apps.catalogue.models import Category
+
+Category = get_model('catalogue', 'Category')
+
+CategoryForm = movenodeform_factory(
+    Category,
+    fields=['name', 'description', 'image', 'metatag_keyworlds',])
