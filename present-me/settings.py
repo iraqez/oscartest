0084988df9ceea3408d319c8dce8ceb9cf3e1986
Iraqez <vladimir.gumenyuk@gmail.com>
2016-12-03 16:07:45 +0200

    dev_modeltranslation

--- present-me/settings.py
+++ present-me/settings.py
@@ -76,9 +76,8 @@ INSTALLED_APPS = [
     'apps.shipping',
     'apps.user',
     'apps.dashboard.catalogue',
-    'apps.catalogue'
                    ])
-#
+
 SITE_ID = 1
 
 MIDDLEWARE_CLASSES = [
@@ -236,7 +235,6 @@ LANGUAGES = (
 )
 LANGUAGE_CODE = 'uk'
 MODELTRANSLATION_LANGUAGES = ('uk', 'ru')
-MODELTRANSLATION_DEFAULT_LANGUAGE = 'uk'
 
 HAYSTACK_CONNECTIONS = {
     'default': {
