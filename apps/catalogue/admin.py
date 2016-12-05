22a335e4a0295e617c6868d2e79d894a62ee045c
iraqez <vladimir.gumenyuk@gmail.com>
2016-12-05 15:15:29 +0200

    django-modeltranslation

--- apps/catalogue/admin.py
+++ apps/catalogue/admin.py
@@ -1,21 +1,21 @@
 from django.contrib import admin
-from modeltranslation.admin import TranslationAdmin
-from .models import Category
-from oscar.apps.catalogue.admin import CategoryAdmin as CoreCategoryAdmin
+from treebeard.admin import TreeAdmin
+from treebeard.forms import movenodeform_factory
+from modeltranslation.admin import TabbedTranslationAdmin
+from .models import Category as CatNew
+#from oscar.apps.catalogue.admin import CategoryAdmin as CoreCategoryAdmin
+from oscar.apps.catalogue.admin import Category as CategoryOld  # noqa
+import apps.catalogue.translation
+
+admin.site.unregister(CategoryOld)
+#admin.site.register(CatNew)
+class CategoryAdminI18n(TreeAdmin, TabbedTranslationAdmin):
+    form = movenodeform_factory(CategoryOld)
+    list_display = ('name', 'name_ru', 'name_uk', 'slug')
+    list_filter = ('name_ru', 'name_uk')
+#admin.site.unregister(CatNew)
+admin.site.register(CatNew, CategoryAdminI18n)
 from oscar.apps.catalogue.admin import *  # noqa
-
-
-class CoreCategoryAdmin(TranslationAdmin):
-
-    class Media:
-        js = (
-            'http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js',
-            'http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js',
-            'tabbed_translation_fields.js',
-        )
-        css = {
-            'screen': ('tabbed_translation_fields.css',),
-        }
-
-from oscar.apps.catalogue.admin import *  # noqa
+#admin.site.unregister(CategoryAdmin)
+#admin.site.register(CategoryAdminI18n)
 
