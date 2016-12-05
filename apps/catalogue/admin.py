3a3cc8d66bbcd2a7f5a4db85f911f9e90acaecb8
iraqez <vladimir.gumenyuk@gmail.com>
2016-12-05 15:18:21 +0200

    django-modeltranslation

--- apps/catalogue/admin.py
+++ apps/catalogue/admin.py
@@ -2,8 +2,7 @@ from django.contrib import admin
 from treebeard.admin import TreeAdmin
 from treebeard.forms import movenodeform_factory
 from modeltranslation.admin import TabbedTranslationAdmin
-from .models import Category as CatNew
-#from oscar.apps.catalogue.admin import CategoryAdmin as CoreCategoryAdmin
+from .models import Category as CategoryNew
 from oscar.apps.catalogue.admin import Category as CategoryOld  # noqa
 import apps.catalogue.translation
 
@@ -13,9 +12,7 @@ class CategoryAdminI18n(TreeAdmin, TabbedTranslationAdmin):
     form = movenodeform_factory(CategoryOld)
     list_display = ('name', 'name_ru', 'name_uk', 'slug')
     list_filter = ('name_ru', 'name_uk')
-#admin.site.unregister(CatNew)
-admin.site.register(CatNew, CategoryAdminI18n)
+
+admin.site.register(CategoryNew, CategoryAdminI18n)
 from oscar.apps.catalogue.admin import *  # noqa
-#admin.site.unregister(CategoryAdmin)
-#admin.site.register(CategoryAdminI18n)
 
