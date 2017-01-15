a7b374a0f82a75c31508058f45de5f817b03c841
Iraqez <vladimir.gumenyuk@gmail.com>
2017-01-15 23:39:35 +0200

    work-local

--- apps/catalogue/models.py
+++ apps/catalogue/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from oscar.core.utils import slugify
 from oscar.apps.catalogue.abstract_models import AbstractCategory, AbstractProductClass,\
     AbstractProduct, AbstractProductAttribute, AbstractProductAttributeValue,\
     AbstractAttributeOptionGroup, AbstractAttributeOption
@@ -16,8 +17,13 @@ class Category(AbstractCategory):
 
 
 class Product(AbstractProduct):
-   metatag_keyworlds = models.CharField(verbose_name=_('Keyword'), max_length=255, null=True)
+    metatag_keyworlds = models.CharField(verbose_name=_('Keyword'), max_length=255, null=True)
 
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.get_title())
+        super(AbstractProduct, self).save(*args, **kwargs)
+        self.attr.save()
 
 class ProductAttribute(AbstractProductAttribute):
     pass
